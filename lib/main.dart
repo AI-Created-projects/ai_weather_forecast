2afa2b269281e32a63a1a377ce3227ed815cfdc3
Martin Jablečník <martin.jablecnik@email.cz>
2025-06-06 13:19:15 +0200

    chore: init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:vader_console/vader_console.dart';
+import 'package:weather_forecast/arguments.dart';
+
+void main(List<String> args) {
+  runCliApp(
+    arguments: args,
+    commands: commands,
+    parser: CliArguments.parse,
+    app: (args) {
+      print("Main part of my app...");
+      print("Message: ${args.message}");
+    },
+  );
+}
